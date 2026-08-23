6be4307df97c3959dd9527f57f49633cbaddfc33
Sergio Tarrifa <114589563+Sergio5423@users.noreply.github.com>
2026-08-23 12:41:32 -0500

    Inicialización de entorno

---
+++ src/test/java/com/unicesar/roomies/RoomiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicesar.roomies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
